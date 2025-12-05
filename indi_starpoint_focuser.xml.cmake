91a1348bb16d75ca25eff5d2f58322a39762822c
dr-washington <97872207+dr-washington@users.noreply.github.com>
2025-12-05 12:05:18 +0800

    Fix device label and driver name in XML configuration

--- indi_starpoint_focuser.xml.cmake
+++ indi_starpoint_focuser.xml.cmake
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
    <devGroup group="Focusers">
-      <device label="Starpoint Focuser" manufacturer="Starpoint Australis">
-         <driver name="STarpoint Focuser Driver">indi_starpoint_focuser_driver/driver>
+      <device name="Starpoint Focuser" label="Starpoint Focuser" manufacturer="Starpoint Australis">
+         <driver name="Sarpoint Focuser Driver">indi_starpoint_focuser</driver>
          <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
       </device>
    </devGroup>
